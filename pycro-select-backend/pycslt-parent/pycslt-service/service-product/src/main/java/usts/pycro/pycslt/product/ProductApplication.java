cc8736232156935351e88531e5b80a2c8733114f
Pycro <pycro6815@163.com>
2023-11-20 10:52:31 +0800

    前台系统-环境搭建 完成

---
+++ pycro-select-backend/pycslt-parent/pycslt-service/service-product/src/main/java/usts/pycro/pycslt/product/ProductApplication.java
@@ -0,0 +1,16 @@
+package usts.pycro.pycslt.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Pycro
+ * @version 1.0
+ * 2023-11-20 10:50
+ */
+@SpringBootApplication
+public class ProductApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class, args);
+    }
+}
